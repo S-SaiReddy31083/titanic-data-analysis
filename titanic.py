13b20e5d9ee12285e5b17e71cd38db321ed50c42
S-SaiReddy31083 <saireddyseelam03@gmail.com>
2026-03-21 13:18:04 +0530

    Performed Titanic data analysis with preprocessing, visualizations, and logistic regression

--- titanic.py
+++ titanic.py
@@ -71,5 +71,43 @@ sns.histplot(df["Age"],bins=20,color="orange")
 plt.title("Age Distribution")
 plt.show()
 
+#Implementing the scikit learn library fot the Titanic dataset
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,classification_report, confusion_matrix
+
+#Input features and target/output variable
+x = df[["Pclass","Age","SibSp","Parch"]]
+y = df["Survived"]
+
+#Splitting the dataset into training and testing sets
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+#Creating the Logistic Regressio model
+model = LogisticRegression()
+model.fit(x_train,y_train)
+
+#Making the Predictions
+y_pred = model.predict(x_test)
+print("Predicted Values: ",y_pred)
+
+#Checking the Accuracy of the model
+print("Accuracy of the model: ",accuracy_score(y_test,y_pred))
+
+#Classification Report
+print("Classification Report: ",classification_report(y_test,y_pred))
+
+#Confusion Matrix
+print("Confusion Matrix: ")
+print(confusion_matrix(y_test,y_pred))
+
+#Graph Representation of the Confusion Matrix using the Scikitlearn Library
+plt.figure(figsize=(6,4))
+sns.heatmap(confusion_matrix(y_test,y_pred),annot = True,fmt="d",cmap="Blues")
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
+
 
 
